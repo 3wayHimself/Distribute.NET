b03f422c0c513265ff1e2031005c628bf4dddbb6
Spanfile <santeri.kasvi@gmail.com>
2015-02-28 11:44:27 +0200

    Master base

---
+++ Master/Command.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Master
+{
+    public static class Command
+    {
+        static Dictionary<string, Action<string[]>> commands;
+
+        static Command()
+        {
+            commands = new Dictionary<string, Action<string[]>>();
+        }
+
+        public static void Register(string name, Action<string[]> action)
+        {
+            if (commands.ContainsKey(name))
+                throw new ArgumentException("Command already registered: " + name);
+
+            commands.Add(name, action);
+        }
+
+        public static bool Run(string name, string[] args)
+        {
+            if (!commands.ContainsKey(name))
+                return false;
+
+            commands[name](args);
+            return true;
+        }
+    }
+}
--- Master/Program.cs
+++ Master/Program.cs
@@ -3,14 +3,105 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Threading;
+using System.IO;
+using Lidgren.Network;
 
 namespace Master
 {
     class Program
     {
+        static Version version;
+
+        static NetServer server;
+
         public static void Main(string[] args)
         {
+            bool running = true;
 
+            if (!File.Exists("version.txt"))
+            {
+                Console.WriteLine("Missing version.txt, assuming default");
+                version = new Version(0, 1, 0);
+            }
+            else
+                version = new Version(File.ReadAllText("version.txt"));
+
+            Console.WriteLine("Distribute.NET Master - " + version.ToString());
+
+            NetPeerConfiguration cfg = new NetPeerConfiguration("Distribute.NET");
+            cfg.EnableMessageType(NetIncomingMessageType.DiscoveryRequest);
+            cfg.Port = 6969;
+
+            server = new NetServer(cfg);
+            server.RegisterReceivedCallback(new SendOrPostCallback(MessageReceived), new SynchronizationContext());
+            server.Start();
+            Console.WriteLine("Server started and listening");
+
+            Command.Register("close", new Action<string[]>((a) => running = false));
+            Command.Register("conns", new Action<string[]>((a) =>
+            {
+                int index = 1;
+                foreach (var connection in server.Connections)
+                {
+                    Console.WriteLine("{0}\t{1}\t{2}", index, connection.RemoteEndPoint.ToString(), connection.AverageRoundtripTime);
+                    index += 1;
+                }
+            }));
+            Command.Register("discover", new Action<string[]>((a) => server.DiscoverLocalPeers(6969)));
+
+            string line;
+            while (running)
+            {
+                Console.Write("> ");
+                line = Console.ReadLine();
+
+                string[] lineArgs = line.Split(' ');
+                if (!Command.Run(lineArgs[0], lineArgs.Skip(1).ToArray()))
+                    Console.WriteLine("Invalid command");
+            }
+
+            Console.WriteLine("Closing...");
+
+            server.Shutdown("bye");
+        }
+
+        static void MessageReceived(object peerObj)
+        {
+            NetPeer peer = (NetPeer)peerObj;
+            NetIncomingMessage inc = peer.ReadMessage();
+
+            switch (inc.MessageType)
+            {
+                case NetIncomingMessageType.DebugMessage:
+                    Console.WriteLine("Lidgren debug: {0}", inc.ReadString());
+                    break;
+
+                case NetIncomingMessageType.WarningMessage:
+                    Console.WriteLine("Lidgren warning: {0}", inc.ReadString());
+                    break;
+
+                case NetIncomingMessageType.ErrorMessage:
+                    Console.WriteLine("Lidgren error: {0}", inc.ReadString());
+                    break;
+
+                case NetIncomingMessageType.Data:
+                    Console.WriteLine("Incoming message from {0}: {1}", inc.SenderEndPoint.ToString(), inc.ReadString());
+                    break;
+
+                case NetIncomingMessageType.DiscoveryRequest:
+                    Console.WriteLine("Discovery request from {0}", inc.SenderEndPoint.ToString());
+
+                    NetOutgoingMessage outMsg = server.CreateMessage();
+                    outMsg.Write("master");
+                    server.SendMessage(outMsg, inc.SenderConnection, NetDeliveryMethod.ReliableOrdered);
+
+                    break;
+
+                case NetIncomingMessageType.DiscoveryResponse:
+                    Console.WriteLine("Found slave at {0}: {1}", inc.SenderEndPoint.ToString(), inc.ReadString());
+                    break;
+            }
         }
     }
 }
